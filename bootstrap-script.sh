8c8288fc4c1c848f6895ce1b6b65493f7d8f4151
Owais Rizvi <68285890+owaisrizvi97@users.noreply.github.com>
2023-09-25 23:41:15 +0500

    Create bootstrap-script.sh

---
+++ bootstrap-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get install fontconfig openjdk-17-jre jenkins -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+sudo chmod 666 /var/run/docker.sock

4bfdd3c8801ce23cf8a530dbf3371957d9b1d8e3
Owais Rizvi <68285890+owaisrizvi97@users.noreply.github.com>
2023-10-06 23:41:02 +0500

    Update bootstrap-script.sh

--- bootstrap-script.sh
+++ bootstrap-script.sh
@@ -2,6 +2,7 @@
 sudo apt update
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-update -y
 sudo apt-get install fontconfig openjdk-17-jre jenkins -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
